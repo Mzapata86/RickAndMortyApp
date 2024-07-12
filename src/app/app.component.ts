a223790d9608b9d634c77665efb35243563a1e3d
Mzapata86 <mpalacioszapata22@gmail.com>
2024-07-12 13:23:22 -0500

    Cambios en el diseño del paginador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,14 @@ import { ToolbarComponent } from "./components/toolbar/toolbar.component";
 import { CardComponent } from "./components/card/card.component";
 import { RickAndMortyService } from './services/rick-and-morty.service';
 import { CharactersPage } from './models/characters-page.interface';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [ToolbarComponent, CardComponent, MatPaginatorModule]
+  imports: [ToolbarComponent, CardComponent]
 })
 export class AppComponent implements OnInit {
   
--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatPaginatorModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
-
+  @Input() charactersCount: number | undefined;
+  @Output() pageChanged: EventEmitter<PageEvent> = new EventEmitter();
 }
